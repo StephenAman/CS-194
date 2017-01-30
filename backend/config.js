52e2ddddcdc4eae98084895e28c12c378fe418be
brygge2 <joachim.reiersen@gmail.com>
2017-01-30 01:30:15 -0800

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
-exports.mysql_host = ""
-exports.mysql_user = ""
-exports.mysql_password = ""
-exports.mysql_database = ""
+exports.mysql_host = "";
+exports.mysql_user = "";
+exports.mysql_password = "";
+exports.mysql_database = "";
 exports.jwt_secret = "";
 exports.fb_client_id = "";
 exports.fb_client_secret = "";
