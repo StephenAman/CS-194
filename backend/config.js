c26c5a15786e0719adc6bc209aabe06632928f83
Joachim Reiersen <joachimr@stanford.edu>
2017-01-30 01:25:45 -0800

    Added config.js without credentials
    Since repo is public

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+exports.mysql_host = ""
+exports.mysql_user = ""
+exports.mysql_password = ""
+exports.mysql_database = ""
+exports.jwt_secret = "";
+exports.fb_client_id = "";
+exports.fb_client_secret = "";
+exports.fb_callback_url = "";
